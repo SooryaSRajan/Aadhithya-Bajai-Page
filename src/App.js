14191e55f2e43dad0b99ed74ca84c9585a4e441c
sooryasrajan <soorya.s27@gmail.com>
2023-05-18 00:20:54 +0530

    Added place holder hero page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
+import './App.scss';
 import NavBar from "./components/nav/NavBar";
+import HeroPage from "./pages/hero/HeroPage";
 
 function App() {
-  return (
-    <div className="app">
-      <NavBar/>
-    </div>
-  );
+    return (
+      <div className="app">
+          <NavBar/>
+          <div className="app-content__root">
+              <HeroPage/>
+          </div>
+      </div>
+    );
 }
 
 export default App;
---
+++ src/pages/hero/HeroPage.js
@@ -0,0 +1,13 @@
+import './HeroPage.scss';
+
+function HeroPage() {
+    return (
+      <div className="hero-page">
+            <div className="hero-page__content">
+
+            </div>
+      </div>
+    );
+}
+
+export default HeroPage;
